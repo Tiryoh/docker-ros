996074649c8e9da3fa10d1447e82e0612d5f8655
Daisuke Sato <tiryoh@gmail.com>
2024-05-06 23:38:55 +0900

    feat: Create user even if home directory is mounted

--- melodic/ros_entrypoint.sh
+++ melodic/ros_entrypoint.sh
@@ -12,12 +12,12 @@ if [[ ! -z "$USER" ]]; then
 	DEFAULT_USER=$USER
 fi
 
-# create development user
 if [[ $(id -u) -eq 0 ]]; then
 	EXEC="exec /sbin/su-exec ${DEFAULT_USER}"
 
-	# create_user
-	if [[ ! -e /home/${DEFAULT_USER} ]]; then
+	# if the user does not exist, create user
+	if [[ $(id -u ${DEFAULT_USER} 2> /dev/null) != ${DEFAULT_USER_UID} ]]; then
+	    echo creating user ${DEFAULT_USER}
 		groupadd -g "${DEFAULT_USER_GID}" "${DEFAULT_USER}"
 		useradd --create-home --home-dir /home/${DEFAULT_USER} --uid ${DEFAULT_USER_UID} --shell /bin/bash \
 		    --gid ${DEFAULT_USER_GID} --groups sudo ${DEFAULT_USER}
@@ -26,30 +26,40 @@ if [[ $(id -u) -eq 0 ]]; then
 		touch /home/${DEFAULT_USER}/.sudo_as_admin_successful
 	fi
 
+	# mount develop workspace
+	if [ -e /ws ]; then
+		mkdir -p /home/${DEFAULT_USER}/catkin_ws
+		ln -s /ws /home/${DEFAULT_USER}/catkin_ws/src
+	fi
+
 	# setup ros environment
 	if [[ "$BUILD_TOOL" == "catkin_make" ]]; then
 		touch /home/${DEFAULT_USER}/.bash_aliases
 		echo "function catkin_make(){(cd ~/catkin_ws && command catkin_make \$@) && source ~/catkin_ws/devel/setup.bash;}" >> /home/${DEFAULT_USER}/.bash_aliases
 		mkdir -p /home/${DEFAULT_USER}/catkin_ws/src \
-		&& /bin/bash -c ". /opt/ros/melodic/setup.bash; catkin_init_workspace /home/${DEFAULT_USER}/catkin_ws/src" > /dev/null
-		echo 'source /opt/ros/melodic/setup.bash' >> /home/${DEFAULT_USER}/.bashrc
+		&& /bin/bash -c ". /opt/ros/noetic/setup.bash; catkin_init_workspace /home/${DEFAULT_USER}/catkin_ws/src" > /dev/null
+		echo 'source /opt/ros/noetic/setup.bash' >> /home/${DEFAULT_USER}/.bashrc
 	fi
 	if [[ "$BUILD_TOOL" == "catkin-tools" ]]; then
 		mkdir -p /home/${DEFAULT_USER}/catkin_ws/src \
-		&& /bin/bash -c "cd /home/${DEFAULT_USER}/catkin_ws;. /opt/ros/melodic/setup.bash; catkin init" > /dev/null
-		echo 'source /opt/ros/melodic/setup.bash' >> /home/${DEFAULT_USER}/.bashrc
+		&& /bin/bash -c "cd /home/${DEFAULT_USER}/catkin_ws;. /opt/ros/noetic/setup.bash; catkin init" > /dev/null
+		echo 'source /opt/ros/noetic/setup.bash' >> /home/${DEFAULT_USER}/.bashrc
 		echo 'source `catkin locate --shell-verbs`' >> /home/${DEFAULT_USER}/.bashrc
+	fi
+
+	if [ ! -e /home/${DEFAULT_USER}/.config/terminator/config ]; then
+		# Avoid org.freedesktop.DBus.Error.Spawn.ExecFailed
+		# https://forums.bunsenlabs.org/viewtopic.php?pid=59732#p59732
+		mkdir -p /home/${DEFAULT_USER}/.config/terminator
+		echo '[global_config]' | tee -a /home/${DEFAULT_USER}/.config/terminator/config > /dev/null
+		echo '    dbus = "False"' | tee -a /home/${DEFAULT_USER}/.config/terminator/config > /dev/null
 	fi
 
 	chown -R ${DEFAULT_USER}:${DEFAULT_USER} /home/${DEFAULT_USER}
 
-	# mount develop workspace
-	if [ -e /ws ]; then
-	ln -s /ws /home/${DEFAULT_USER}/catkin_ws/src
-	fi
-
 	DEFAULT_USER_UID="$(${EXEC} id -u)"
 	DEFAULT_USER_GID="$(${EXEC} id -g)"
+	HOME="/home/${DEFAULT_USER}"
 else # use existing user
 	EXEC="exec"
 	DEFAULT_USER="$(whoami)"
@@ -57,18 +67,12 @@ else # use existing user
 	DEFAULT_USER_GID="$(id -g)"
 fi
 
-if [ ! -e /home/${DEFAULT_USER}/.config/terminator/config ]; then
-	# Avoid org.freedesktop.DBus.Error.Spawn.ExecFailed
-	# https://forums.bunsenlabs.org/viewtopic.php?pid=59732#p59732
-	mkdir -p /home/${DEFAULT_USER}/.config/terminator
-	echo '[global_config]' | tee -a /home/${DEFAULT_USER}/.config/terminator/config > /dev/null
-	echo '    dbus = "False"' | tee -a /home/${DEFAULT_USER}/.config/terminator/config > /dev/null
-fi
+echo "Launched container with user: ${DEFAULT_USER}, uid: ${DEFAULT_USER_UID}, gid: ${DEFAULT_USER_GID}"
 
-echo "Launched container with user: $DEFAULT_USER, uid: $DEFAULT_USER_UID, gid: $DEFAULT_USER_GID"
+cd ${HOME}
 
 if which "$1" > /dev/null 2>&1 ; then
-	$EXEC "$@"
+	${EXEC} "$@"
 else
-	echo $@ | $EXEC $SHELL -li
+	echo $@ | ${EXEC} ${SHELL} -li
 fi

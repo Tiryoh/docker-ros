d6bc74dacbdda4bf1f77f16c7ae5fac41a3d6a04
Daisuke Sato <tiryoh@gmail.com>
2024-06-05 00:59:52 +0900

    feat: Double quote to prevent globbing (SC2086) (#24)

--- run.sh
+++ run.sh
@@ -8,32 +8,31 @@ DEFAULT_USER=ubuntu
 
 # ディレクトリへの絶対パスが異なる場合は別の名前でコンテナを立ち上げる（共有PCや異なるROSDistroでの利用を想定）
 command -v crc32 >/dev/null || { echo ERROR: crc32 command not found. Try "sudo apt-get update && sudo apt-get install -y libarchive-zip-perl"; exit 1; }
-CONTAINER_NAME="ros_${ROS_DISTRO}_$(crc32 <(echo ${WS_ROOT_DIR}))"
+CONTAINER_NAME="ros_${ROS_DISTRO}_$(crc32 <(echo "${WS_ROOT_DIR}"))"
 
 # コンテナがすでに起動しているかどうか確認
-if docker ps -a | grep -q ${CONTAINER_NAME}; then
-        echo Found container: ${CONTAINER_NAME}
+if docker ps -a | grep -q "${CONTAINER_NAME}"; then
+        echo Found container: "${CONTAINER_NAME}"
         # 引数が何もなければshellをインタラクティブモードで起動する
         if [ "$#" == 0 ]; then
-                docker exec -it ${CONTAINER_NAME} ${DEFAULT_SHELL}
+                docker exec -it "${CONTAINER_NAME}" "${DEFAULT_SHELL}"
         else
-                docker exec -it ${CONTAINER_NAME} "$@"
+                docker exec -it "${CONTAINER_NAME}" "$@"
         fi
 
 else
-        echo Starting container: ${CONTAINER_NAME}
+        echo Starting container: "${CONTAINER_NAME}"
         # コンテナが起動していなければ、新規に立ち上げる
         docker run --rm -it \
                 --privileged \
                 --ipc=host \
                 --net=host \
-                --name ${CONTAINER_NAME} \
-                -e CONTAINER_NAME=${CONTAINER_NAME} \
+                --name "${CONTAINER_NAME}" \
+                -e CONTAINER_NAME="${CONTAINER_NAME}" \
                 -e DISPLAY \
                 -v /tmp/.X11-unix:/tmp/.X11-unix \
-                -v /etc/localtime:/etc/localtime \
-                -v ${HOME}/catkin_ws/src:/ws \
-                ${DOCKER_IMAGE_NAME} \
+                -v /etc/localtime:/etc/localtime:ro \
+                -v "${HOME}/catkin_ws/src:/ws" \
+                "${DOCKER_IMAGE_NAME}" \
                 "$@"
 fi
-

05a4757ef89e75f4e335ce7d82b0d27b486b2f40
Daisuke Sato <tiryoh@gmail.com>
2024-11-25 20:02:41 +0900

    feat: Select default shell (#32)

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ ROS_DISTRO=${ROS_DISTRO:-noetic}
 DOCKER_IMAGE_NAME="tiryoh/ros:${ROS_DISTRO}"
 DEFAULT_SHELL=bash
 DEFAULT_USER=ubuntu
+DEFAULT_SHELL="/bin/bash"
+# DEFAULT_SHELL="/usr/bin/zsh"
 
 # ディレクトリへの絶対パスが異なる場合は別の名前でコンテナを立ち上げる（共有PCや異なるROSDistroでの利用を想定）
 command -v crc32 >/dev/null || { echo ERROR: crc32 command not found. Try "sudo apt-get update && sudo apt-get install -y libarchive-zip-perl"; exit 1; }
@@ -21,6 +23,11 @@ if docker ps -a | grep -q "${CONTAINER_NAME}"; then
         fi
 
 else
+	if [ "$#" == 0 ]; then
+		CMD="${DEFAULT_SHELL}"
+	else
+		CMD="$@"
+	fi
         echo Starting container: "${CONTAINER_NAME}"
         # コンテナが起動していなければ、新規に立ち上げる
         docker run --rm -it \
@@ -30,9 +37,15 @@ else
                 --name "${CONTAINER_NAME}" \
                 -e CONTAINER_NAME="${CONTAINER_NAME}" \
                 -e DISPLAY \
+		-e TERM=xterm-256color \
+		-e DISPLAY \
+		-e DEFAULT_SHELL \
+		-e DEFAULT_USER \
                 -v /tmp/.X11-unix:/tmp/.X11-unix \
                 -v /etc/localtime:/etc/localtime:ro \
+		-v /etc/timezone:/etc/timezone:ro \
+		-v "${HOME}/.local/share/fonts:/home/${USER}/.local/share/fonts" \
                 -v "${HOME}/catkin_ws/src:/ws" \
                 "${DOCKER_IMAGE_NAME}" \
-                "$@"
+                "${CMD}"
 fi
